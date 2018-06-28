82171d66597c26ff2b6bd409f05c0423953c8c70
Yuva <yuva@codemancers.com>
2018-06-28 23:04:17 +0530

    add support for interacting with mysql

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule DbPool.Mixfile do
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
+      {:mariaex, "~> 0.8.4"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
