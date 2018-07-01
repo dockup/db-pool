c18eecc1614a5c39c17b461cecd265f038264d81
Yuva <yuva@codemancers.com>
2018-07-02 01:15:52 +0530

    add distillery and run release.init

--- mix.exs
+++ mix.exs
@@ -41,6 +41,7 @@ defmodule DbPool.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
+      {:distillery, "~> 1.5"},
       {:cowboy, "~> 1.0"}
     ]
   end
